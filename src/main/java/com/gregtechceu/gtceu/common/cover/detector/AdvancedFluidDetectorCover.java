419a781a30cdf79e832250870cc02b0eb6b30719
Spicierspace153 <cameronjmiller@outlook.com>
2024-11-13 22:49:05 -0700

    Adds RS latch for advanced item and fluid detectors (#2338)
    Co-authored-by: Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>

--- src/main/java/com/gregtechceu/gtceu/common/cover/detector/AdvancedFluidDetectorCover.java
+++ src/main/java/com/gregtechceu/gtceu/common/cover/detector/AdvancedFluidDetectorCover.java
@@ -29,6 +29,7 @@ import net.neoforged.neoforge.fluids.FluidStack;
 import net.neoforged.neoforge.fluids.capability.IFluidHandler;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -48,7 +49,10 @@ public class AdvancedFluidDetectorCover extends FluidDetectorCover implements IU
 
     private static final int DEFAULT_MIN = 64;
     private static final int DEFAULT_MAX = 512;
-
+    @Persisted
+    @Getter
+    @Setter
+    private int outputAmount;
     @Persisted
     @Getter
     private long minValue, maxValue;
@@ -96,7 +100,8 @@ public class AdvancedFluidDetectorCover extends FluidDetectorCover implements IU
         }
 
         setRedstoneSignalOutput(
-                RedstoneUtil.computeRedstoneBetweenValues(storedFluid, maxValue, minValue, this.isInverted()));
+                this.outputAmount = RedstoneUtil.computeLatchedRedstoneBetweenValues(storedFluid, maxValue, minValue,
+                        isInverted(), this.outputAmount));
     }
 
     public void setMinValue(long minValue) {

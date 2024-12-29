391d543d7b8bf5fb6dbf638bc4ce5859a79451f8
Stanley Goodwin <github@staniel.dev>
2024-12-29 14:34:07 -0500

    Update ActiveBlock.java

--- src/main/java/com/gregtechceu/gtceu/api/block/ActiveBlock.java
+++ src/main/java/com/gregtechceu/gtceu/api/block/ActiveBlock.java
@@ -11,34 +11,76 @@ import net.minecraft.world.level.block.state.properties.BooleanProperty;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
+/**
+ * @implNote This class is a simple extension of AppearanceBlock that adds a property
+ * to track whether the block is active or not.
+ * This is useful for blocks that have different appearances when active.
+ */
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class ActiveBlock extends AppearanceBlock {
-
+    /**
+     * This class is a simple extension of AppearanceBlock that adds a
+     * property to track whether the block is active or not.
+     * This is useful for blocks that have different appearances when active.
+     */
     public static final BooleanProperty ACTIVE = BooleanProperty.create("active");
 
+    /**
+     * Constructor for ActiveBlock
+     * @param properties the properties of the block
+     */
     public ActiveBlock(Properties properties) {
         super(properties);
-        registerDefaultState(defaultBlockState().setValue(ACTIVE, false));
+        registerDefaultState(
+                defaultBlockState().setValue(ACTIVE, false)
+        );
     }
 
+    /**
+     * Adds the ACTIVE property to the block state
+     * @param builder the block state builder
+     */
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         super.createBlockStateDefinition(builder);
         builder.add(ACTIVE);
     }
 
+    /**
+     * Changes the active state of the block
+     * @param state the block state
+     * @param active whether the block is active or not
+     * @return the new block state
+     */
     public BlockState changeActive(BlockState state, boolean active) {
         if (state.is(this)) {
             return state.setValue(ACTIVE, active);
         }
-        return state;
+        else {
+            return state;
+        }
     }
 
+    /**
+     * Gets the active state of the block
+     * @param state the block state
+     * @return whether the block is active or not
+     */
     public boolean isActive(BlockState state) {
         return state.getValue(ACTIVE);
     }
 
+    /**
+     * Gets the block appearance based on the active state
+     * @param state the block state
+     * @param level the block and tint getter
+     * @param pos the block position
+     * @param side the direction of the block
+     * @param sourceState the source block state
+     * @param sourcePos the source block position
+     * @return the block state
+     */
     @Override
     public BlockState getBlockAppearance(BlockState state, BlockAndTintGetter level, BlockPos pos, Direction side,
                                          BlockState sourceState, BlockPos sourcePos) {

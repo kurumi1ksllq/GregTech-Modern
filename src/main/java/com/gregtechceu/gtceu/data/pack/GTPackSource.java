647672ec31198d8a27f7fc34eb8fffd4c9482997
screret <68943070+screret@users.noreply.github.com>
2025-05-25 14:30:19 +0300

    oh, I had a better solution for this

--- src/main/java/com/gregtechceu/gtceu/data/pack/GTPackSource.java
+++ src/main/java/com/gregtechceu/gtceu/data/pack/GTPackSource.java
@@ -1,7 +1,6 @@
 package com.gregtechceu.gtceu.data.pack;
 
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.PackSource;
@@ -10,7 +9,6 @@ import net.minecraft.server.packs.repository.RepositorySource;
 import lombok.RequiredArgsConstructor;
 
 import java.util.function.Consumer;
-import java.util.function.Function;
 
 @RequiredArgsConstructor
 public class GTPackSource implements RepositorySource {
@@ -18,23 +16,20 @@ public class GTPackSource implements RepositorySource {
     private final String name;
     private final PackType type;
     private final Pack.Position position;
-    private final Function<String, PackResources> resources;
-
-    @Override
-    public void loadPacks(Consumer<Pack> onLoad) {
-        onLoad.accept(createFixed(name,
-                Component.literal(name),
-                true,
-                resources::apply,
-                type,
-                position,
-                PackSource.BUILT_IN));
-    }
+    private final Pack.ResourcesSupplier resources;
 
     @SuppressWarnings("DataFlowIssue")
-    public static Pack createFixed(String id, Component title, boolean required, Pack.ResourcesSupplier resources,
-                                   PackType packType, Pack.Position defaultPosition, PackSource packSource) {
-        return Pack.create(id, title, required, resources,
-                Pack.readPackInfo(id, resources), packType, defaultPosition, true, packSource);
+    @Override
+    public void loadPacks(Consumer<Pack> onLoad) {
+        Pack.Info info = Pack.readPackInfo(name, resources);
+        onLoad.accept(Pack.create(name,
+                Component.literal(name),
+                true,
+                resources,
+                info,
+                type,
+                position,
+                true,
+                PackSource.BUILT_IN));
     }
 }

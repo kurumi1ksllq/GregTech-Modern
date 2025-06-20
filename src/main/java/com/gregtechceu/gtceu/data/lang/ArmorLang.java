6f83930795383074bdf2ca4bd330857997b3d41b
Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>
2025-06-19 20:33:13 -0600

    armorLang.java cleanup

--- src/main/java/com/gregtechceu/gtceu/data/lang/ArmorLang.java
+++ src/main/java/com/gregtechceu/gtceu/data/lang/ArmorLang.java
@@ -7,52 +7,76 @@ import static com.gregtechceu.gtceu.data.lang.LangUtil.*;
 public class ArmorLang {
     public static void generateArmorKeys(RegistrateLangProvider provider) {
 
+
+        // nanomuscle suit
+        provider.add("armor.gtceu.nms.nightvision.enabled", "NanoMuscle™ Suite: NightVision Enabled");
+        provider.add("armor.gtceu.nms.nightvision.disabled", "NanoMuscle™ Suite: NightVision Disabled");
+        provider.add("armor.gtceu.nms.boosted_jump.enabled", "NanoMuscle™ Suite: Jump Boost Enabled");
+        provider.add("armor.gtceu.nms.boosted_jump.disabled", "NanoMuscle™ Suite: Jump Boost Disabled");
+        provider.add("armor.gtceu.nms.nightvision.error", "NanoMuscle™ Suite: §cNot enough power!");
+        provider.add("armor.gtceu.nms.share.enable", "NanoMuscle™ Suite: Charging Enabled");
+        provider.add("armor.gtceu.nms.share.disable", "NanoMuscle™ Suite: Charging Disabled");
+        provider.add("armor.gtceu.nms.share.error", "NanoMuscle™ Suite: §cNot enough power for charging!");
+
+
+        // quantum suit
+        provider.add("armor.gtceu.qts.nightvision.enabled", "QuarkTech™ Suite: NightVision Enabled");
+        provider.add("armor.gtceu.qts.nightvision.disabled", "QuarkTech™ Suite: NightVision Disabled");
+        provider.add("armor.gtceu.qts.nightvision.error", "QuarkTech™ Suite: §cNot enough power!");
+        provider.add("armor.gtceu.qts.share.enable", "QuarkTech™ Suite: Charging Enabled");
+        provider.add("armor.gtceu.qts.share.disable", "QuarkTech™ Suite: Charging Disabled");
+        provider.add("armor.gtceu.qts.share.error", "QuarkTech™ Suite: §cNot enough power for charging!");
+
+
+        //jetpacks
+        provider.add("armor.gtceu.jetpack.flight.enable", "Jetpack: Flight Enabled");
+        provider.add("armor.gtceu.jetpack.flight.disable", "Jetpack: Flight Disabled");
+        provider.add("armor.gtceu.jetpack.hover.enable", "Jetpack: Hover Mode Enabled");
+        provider.add("armor.gtceu.jetpack.hover.disable", "Jetpack: Hover Mode Disabled");
+        provider.add("armor.gtceu.jetpack.emergency_hover_mode", "Emergency Hover Mode Enabled!");
         provider.add("item.liquid_fuel_jetpack.tooltip", "Uses Combustion Generator Fuels for Thrust");
-        provider.add("metaarmor.nms.nightvision.enabled", "NanoMuscle™ Suite: NightVision Enabled");
-        provider.add("metaarmor.nms.nightvision.disabled", "NanoMuscle™ Suite: NightVision Disabled");
-        provider.add("metaarmor.nms.boosted_jump.enabled", "NanoMuscle™ Suite: Jump Boost Enabled");
-        provider.add("metaarmor.nms.boosted_jump.disabled", "NanoMuscle™ Suite: Jump Boost Disabled");
-        provider.add("metaarmor.nms.nightvision.error", "NanoMuscle™ Suite: §cNot enough power!");
-        provider.add("metaarmor.qts.nightvision.enabled", "QuarkTech™ Suite: NightVision Enabled");
-        provider.add("metaarmor.qts.nightvision.disabled", "QuarkTech™ Suite: NightVision Disabled");
-        provider.add("metaarmor.qts.nightvision.error", "QuarkTech™ Suite: §cNot enough power!");
-        provider.add("metaarmor.jetpack.flight.enable", "Jetpack: Flight Enabled");
-        provider.add("metaarmor.jetpack.flight.disable", "Jetpack: Flight Disabled");
-        provider.add("metaarmor.jetpack.hover.enable", "Jetpack: Hover Mode Enabled");
-        provider.add("metaarmor.jetpack.hover.disable", "Jetpack: Hover Mode Disabled");
-        provider.add("metaarmor.jetpack.emergency_hover_mode", "Emergency Hover Mode Enabled!");
-        provider.add("metaarmor.nms.share.enable", "NanoMuscle™ Suite: Charging Enabled");
-        provider.add("metaarmor.nms.share.disable", "NanoMuscle™ Suite: Charging Disabled");
-        provider.add("metaarmor.nms.share.error", "NanoMuscle™ Suite: §cNot enough power for charging!");
-        provider.add("metaarmor.qts.share.enable", "QuarkTech™ Suite: Charging Enabled");
-        provider.add("metaarmor.qts.share.disable", "QuarkTech™ Suite: Charging Disabled");
-        provider.add("metaarmor.qts.share.error", "QuarkTech™ Suite: §cNot enough power for charging!");
-        provider.add("metaarmor.message.nightvision.enabled", "§bNightVision: §aOn");
-        provider.add("metaarmor.message.nightvision.disabled", "§bNightVision: §cOff");
-        provider.add("metaarmor.message.nightvision.error", "§cNot enough power!");
-        provider.add("metaarmor.tooltip.stepassist", "Provides Step-Assist");
-        provider.add("metaarmor.tooltip.speed", "Increases Running Speed");
-        provider.add("metaarmor.tooltip.jump", "Increases Jump Height and Distance");
-        provider.add("metaarmor.tooltip.falldamage", "Nullifies Fall Damage");
-        provider.add("metaarmor.tooltip.potions", "Nullifies Harmful Effects");
-        provider.add("metaarmor.tooltip.burning", "Nullifies Burning");
-        provider.add("metaarmor.tooltip.freezing", "Prevents Freezing");
-        provider.add("metaarmor.tooltip.breath", "Replenishes Underwater Breath Bar");
-        provider.add("metaarmor.tooltip.autoeat", "Replenishes Food Bar by Using Food from Inventory");
-        provider.add("metaarmor.hud.status.enabled", "§aON");
-        provider.add("metaarmor.hud.status.disabled", "§cOFF");
-        provider.add("metaarmor.hud.energy_lvl", "Energy Level: %s");
-        provider.add("metaarmor.hud.engine_enabled", "Engine Enabled: %s");
-        provider.add("metaarmor.hud.fuel_lvl", "Fuel Level: %s");
-        provider.add("metaarmor.hud.hover_mode", "Hover Mode: %s");
-        provider.add("mataarmor.hud.supply_mode", "Supply Mode: %s");
-        provider.add("metaarmor.hud.gravi_engine", "GraviEngine: %s");
-        provider.add("metaarmor.energy_share.error", "Energy Supply: §cNot enough power for gadgets charging!");
-        provider.add("metaarmor.energy_share.enable", "Energy Supply: Gadgets charging enabled");
-        provider.add("metaarmor.energy_share.disable", "Energy Supply: Gadgets charging disabled");
-        provider.add("metaarmor.energy_share.tooltip", "Supply mode: %s");
-        provider.add("metaarmor.energy_share.tooltip.guide",
+
+        //action bar messages
+        provider.add("armor.gtceu.message.nightvision.enabled", "§bNightVision: §aOn");
+        provider.add("armor.gtceu.message.nightvision.disabled", "§bNightVision: §cOff");
+        provider.add("armor.gtceu.message.nightvision.error", "§cNot enough power!");
+
+
+     }
+
+    public static void generateTooltipKeys(RegistrateLangProvider provider){
+
+        // basic ability tooltips
+        provider.add("armor.gtceu.tooltip.stepassist", "Provides Step-Assist");
+        provider.add("armor.gtceu.tooltip.speed", "Increases Running Speed");
+        provider.add("armor.gtceu.tooltip.jump", "Increases Jump Height and Distance");
+        provider.add("armor.gtceu.tooltip.falldamage", "Nullifies Fall Damage");
+        provider.add("armor.gtceu.tooltip.potions", "Nullifies Harmful Effects");
+        provider.add("armor.gtceu.tooltip.burning", "Nullifies Burning");
+        provider.add("armor.gtceu.tooltip.freezing", "Prevents Freezing");
+        provider.add("armor.gtceu.tooltip.breath", "Replenishes Underwater Breath Bar");
+        provider.add("armor.gtceu.tooltip.autoeat", "Replenishes Food Bar by Using Food from Inventory");
+
+
+        //energy tooltips (iaddinfo)
+        provider.add("armor.gtceu.energy_share.error", "Energy Supply: §cNot enough power for gadgets charging!");
+        provider.add("armor.gtceu.energy_share.enable", "Energy Supply: Gadgets charging enabled");
+        provider.add("armor.gtceu.energy_share.disable", "Energy Supply: Gadgets charging disabled");
+        provider.add("armor.gtceu.energy_share.tooltip", "Supply mode: %s");
+        provider.add("armor.gtceu.energy_share.tooltip.guide",
                 "To change mode shift-right click when holding item");
+    }
+
+    public static void generateHudKeys(RegistrateLangProvider provider){
+
+        provider.add("armor.gtceu.hud.status.enabled", "§aON");
+        provider.add("armor.gtceu.hud.status.disabled", "§cOFF");
+        provider.add("armor.gtceu.hud.energy_lvl", "Energy Level: %s");
+        provider.add("armor.gtceu.hud.engine_enabled", "Engine Enabled: %s");
+        provider.add("armor.gtceu.hud.fuel_lvl", "Fuel Level: %s");
+        provider.add("armor.gtceu.hud.hover_mode", "Hover Mode: %s");
+        provider.add("mataarmor.hud.supply_mode", "Supply Mode: %s");
+        provider.add("armor.gtceu.hud.gravi_engine", "GraviEngine: %s");
 
     }
 }

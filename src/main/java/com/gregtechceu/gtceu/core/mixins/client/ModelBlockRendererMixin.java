3283b244ffadf3f02e5dc07f7668f482c7e07e08
screret <68943070+screret@users.noreply.github.com>
2025-07-18 20:43:47 +0300

    deduplicate mixin code

--- src/main/java/com/gregtechceu/gtceu/core/mixins/client/ModelBlockRendererMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/client/ModelBlockRendererMixin.java
@@ -4,7 +4,6 @@ import com.gregtechceu.gtceu.client.bloom.BloomEffectUtil;
 
 import net.minecraft.client.renderer.block.ModelBlockRenderer;
 import net.minecraft.client.renderer.block.model.BakedQuad;
-import net.minecraft.core.BlockPos;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
@@ -18,20 +17,11 @@ public class ModelBlockRendererMixin {
     @WrapOperation(method = "putQuadData",
                    at = @At(value = "INVOKE",
                             target = "Lcom/mojang/blaze3d/vertex/VertexConsumer;putBulkData(Lcom/mojang/blaze3d/vertex/PoseStack$Pose;Lnet/minecraft/client/renderer/block/model/BakedQuad;[FFFF[IIZ)V"))
-    private void gtceu$renderToEmissiveBuffer(VertexConsumer instance, PoseStack.Pose poseEntry, BakedQuad quad,
-                                              float[] colorMuls, float red, float green, float blue,
-                                              int[] combinedLights, int combinedOverlay, boolean mulColor,
-                                              Operation<Void> original) {
-        BlockPos chunkOrigin = BloomEffectUtil.CURRENT_RENDERING_CHUNK_POS.get();
-        // Check if quad is full brightness OR we have bloom enabled for the quad
-        if (GTShaders.allowedShader() && chunkOrigin != null &&
-                (!quad.isShade() || GTMetadataSection.hasBloom(quad.getSprite()))) {
-            original.call(BloomEffectUtil.getOrStartBloomBuffer(chunkOrigin), poseEntry, quad,
-                    colorMuls, red, green, blue,
-                    combinedLights, combinedOverlay, mulColor);
-        }
-        original.call(instance, poseEntry, quad,
-                colorMuls, red, green, blue,
-                combinedLights, combinedOverlay, mulColor);
+    private void gtceu$copyToBloomBuffer(VertexConsumer consumer, PoseStack.Pose pose, BakedQuad quad,
+                                         float[] colorMuls, float red, float green, float blue,
+                                         int[] combinedLights, int combinedOverlay, boolean mulColor,
+                                         Operation<Void> original) {
+        BloomEffectUtil.copyToBloomBuffer(consumer, pose, quad, colorMuls, red, green, blue,
+                combinedLights, combinedOverlay, mulColor, original);
     }
 }
--- src/main/java/com/gregtechceu/gtceu/core/mixins/forge/QuadLighterMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/forge/QuadLighterMixin.java
@@ -3,7 +3,6 @@ package com.gregtechceu.gtceu.core.mixins.forge;
 import com.gregtechceu.gtceu.client.bloom.BloomEffectUtil;
 
 import net.minecraft.client.renderer.block.model.BakedQuad;
-import net.minecraft.core.BlockPos;
 import net.minecraftforge.client.model.lighting.QuadLighter;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
@@ -19,20 +18,11 @@ public class QuadLighterMixin {
                    at = @At(value = "INVOKE",
                             target = "Lcom/mojang/blaze3d/vertex/VertexConsumer;putBulkData(Lcom/mojang/blaze3d/vertex/PoseStack$Pose;Lnet/minecraft/client/renderer/block/model/BakedQuad;[FFFF[IIZ)V",
                             remap = true))
-    private void gtceu$renderToEmissiveBuffer(VertexConsumer instance, PoseStack.Pose poseEntry, BakedQuad quad,
-                                              float[] colorMuls, float red, float green, float blue,
-                                              int[] combinedLights, int combinedOverlay, boolean mulColor,
-                                              Operation<Void> original) {
-        BlockPos chunkOrigin = BloomEffectUtil.CURRENT_RENDERING_CHUNK_POS.get();
-        // Check if quad is full brightness OR we have bloom enabled for the quad
-        if (GTShaders.allowedShader() && chunkOrigin != null &&
-                (!quad.isShade() || GTMetadataSection.hasBloom(quad.getSprite()))) {
-            original.call(BloomEffectUtil.getOrStartBloomBuffer(chunkOrigin), poseEntry, quad,
-                    colorMuls, red, green, blue,
-                    combinedLights, combinedOverlay, mulColor);
-        }
-        original.call(instance, poseEntry, quad,
-                colorMuls, red, green, blue,
-                combinedLights, combinedOverlay, mulColor);
+    private void gtceu$copyToBloomBuffer(VertexConsumer consumer, PoseStack.Pose pose, BakedQuad quad,
+                                         float[] colorMuls, float red, float green, float blue,
+                                         int[] combinedLights, int combinedOverlay, boolean mulColor,
+                                         Operation<Void> original) {
+        BloomEffectUtil.copyToBloomBuffer(consumer, pose, quad, colorMuls, red, green, blue,
+                combinedLights, combinedOverlay, mulColor, original);
     }
 }

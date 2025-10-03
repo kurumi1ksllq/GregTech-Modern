b1528d750207ba2063dd2f1dac899d4848e6a056
TarLaboratories <levtaranov3@gmail.com>
2025-10-03 16:56:39 +0300

    oops

--- src/main/java/com/gregtechceu/gtceu/api/events/RegisterGTMachineEvent.java
+++ src/main/java/com/gregtechceu/gtceu/api/events/RegisterGTMachineEvent.java
@@ -3,10 +3,11 @@ package com.gregtechceu.gtceu.api.events;
 import com.gregtechceu.gtceu.api.registry.registrate.MachineBuilder;
 
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.fml.event.IModBusEvent;
 
 import lombok.Getter;
 
-public class RegisterGTMachineEvent extends Event {
+public class RegisterGTMachineEvent extends Event implements IModBusEvent {
 
     @Getter
     private final MachineBuilder<?> builder;

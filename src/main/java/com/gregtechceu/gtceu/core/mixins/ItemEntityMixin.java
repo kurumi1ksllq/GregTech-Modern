87c39bf1e2d7780be53a590e2b5b3c1bdb333679
TarLaboratories <levtaranov3@gmail.com>
2026-01-07 09:51:00 +0300

    no more injecting into getItem :)

--- src/main/java/com/gregtechceu/gtceu/core/mixins/ItemEntityMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/ItemEntityMixin.java
@@ -1,21 +1,28 @@
-package com.gregtechceu.gtceu.core.mixins.client;
+package com.gregtechceu.gtceu.core.mixins;
 
 import com.gregtechceu.gtceu.api.GTValues;
+import com.gregtechceu.gtceu.api.capability.forge.GTCapability;
 import com.gregtechceu.gtceu.api.data.chemical.ChemicalHelper;
 import com.gregtechceu.gtceu.api.data.tag.TagPrefix;
+import com.gregtechceu.gtceu.api.item.component.SpoilContext;
 import com.gregtechceu.gtceu.common.data.GTSoundEntries;
 
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemEntity.class)
-public class ItemEntityMixin {
+public abstract class ItemEntityMixin {
+
+    @Shadow
+    public abstract ItemStack getItem();
 
     @Unique
     private boolean gtceu$wasOnGround = ((Entity) (Object) this).onGround();
@@ -28,5 +35,8 @@ public class ItemEntityMixin {
             GTSoundEntries.METAL_PIPE.playFrom(entity, 10, 1);
         }
         gtceu$wasOnGround = entity.onGround();
+
+        getItem().getCapability(GTCapability.CAPABILITY_SPOILABLE_ITEM)
+                .ifPresent(spoilable -> spoilable.updateFreshness(new SpoilContext(entity), true));
     }
 }

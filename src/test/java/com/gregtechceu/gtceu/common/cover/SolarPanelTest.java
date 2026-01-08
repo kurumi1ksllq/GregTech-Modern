8f1cc9abc9a1dd65f6bd8ed3150cf7b36598a0ea
jurrejelle <jurre@jilles.com>
2026-01-08 20:07:56 +0100

    Fix for the solar tests

--- src/test/java/com/gregtechceu/gtceu/common/cover/SolarPanelTest.java
+++ src/test/java/com/gregtechceu/gtceu/common/cover/SolarPanelTest.java
@@ -4,8 +4,10 @@ import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.common.machine.electric.BatteryBufferMachine;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.gametest.framework.BeforeBatch;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.gametest.GameTestHolder;
 import net.minecraftforge.gametest.PrefixGameTestTemplate;
@@ -18,9 +20,13 @@ public class SolarPanelTest {
         return (BatteryBufferMachine) helper.getBlockEntity(new BlockPos(0, 1, 0));
     }
 
-    @GameTest(template = "solar", batch = "coverTests")
+    @BeforeBatch(batch = "solarTests")
+    public static void prepare(ServerLevel level) {
+        level.setDayTime(6000);
+    }
+
+    @GameTest(template = "solar", batch = "solarTests")
     public static void generatesEnergyAtDayTest(GameTestHelper helper) {
-        helper.setDayTime(6000);
         BatteryBufferMachine machine = getBatteryBuffer(helper);
         machine.onLoad();
         for (int y = helper.absolutePos(new BlockPos(0, 2, 0)).getY(); y < helper.getLevel().getMaxBuildHeight(); y++) {
@@ -33,9 +39,8 @@ public class SolarPanelTest {
         });
     }
 
-    @GameTest(template = "solar", batch = "coverTests")
+    @GameTest(template = "solar", batch = "solarTests")
     public static void doesntGenerateEnergyAtDayWhenBlockedTest(GameTestHelper helper) {
-        helper.setDayTime(6000);
         BatteryBufferMachine machine = getBatteryBuffer(helper);
         machine.onLoad();
         helper.setBlock(new BlockPos(0, 3, 0), Blocks.DIAMOND_BLOCK);

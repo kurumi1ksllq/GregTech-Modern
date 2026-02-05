CleanroomMC/ModularUI

fix nested viewport hover issue

src/main/java/com/cleanroommc/modularui/utils/HoveredWidgetList.java
@@ -1,8 +1,8 @@
 package com.cleanroommc.modularui.utils;
 
+import com.cleanroommc.modularui.api.layout.IViewportStack;
 import com.cleanroommc.modularui.api.widget.IWidget;
 import com.cleanroommc.modularui.screen.viewport.LocatedWidget;
-import com.cleanroommc.modularui.screen.viewport.TransformationMatrix;
 
 import org.jetbrains.annotations.Nullable;
 
@@ -14,8 +14,8 @@ public class HoveredWidgetList {
         this.delegate = delegate;
     }
 
-    public void add(IWidget widget, TransformationMatrix viewports, Object additionalHoverInfo) {
-        this.delegate.addFirst(new LocatedWidget(widget, viewports, additionalHoverInfo));
+    public void add(IWidget widget, IViewportStack viewports, Object additionalHoverInfo) {
+        this.delegate.addFirst(new LocatedWidget(widget, viewports.peek(), additionalHoverInfo));
     }
 
     @Nullable

kurumi1ksllq/GregTech-Modern

(cherry picked from commit c9871109d7f77f0397eef22db2a20f732f6d51bc)

src/main/java/com/cleanroommc/modularui/utils/HoveredWidgetList.java
@@ -1,8 +1,8 @@
 package com.cleanroommc.modularui.utils;
 
 import com.cleanroommc.modularui.base.widget.IWidget;
+import com.cleanroommc.modularui.base.layout.IViewportStack;
 import com.cleanroommc.modularui.screen.viewport.LocatedWidget;
-import com.cleanroommc.modularui.screen.viewport.TransformationMatrix;
 
 import org.jetbrains.annotations.Nullable;
 
@@ -14,8 +14,8 @@ public class HoveredWidgetList {
         this.delegate = delegate;
     }
 
-    public void add(IWidget widget, TransformationMatrix viewports, Object additionalHoverInfo) {
-        this.delegate.add(0, new LocatedWidget(widget, viewports, additionalHoverInfo));
+    public void add(IWidget widget, IViewportStack viewports, Object additionalHoverInfo) {
+        this.delegate.addFirst(new LocatedWidget(widget, viewports.peek(), additionalHoverInfo));
     }
 
     @Nullable